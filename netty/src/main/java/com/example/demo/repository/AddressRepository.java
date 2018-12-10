b0ff87141bb0c1bf2004395799293b79097e1f07
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2018-12-10 12:16:51 +0800

    删除多余

--- netty/src/main/java/com/example/demo/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domain.UserDeliveryAddress;
-
-public interface AddressRepository extends JpaRepository<UserDeliveryAddress, String>{
-
-}
