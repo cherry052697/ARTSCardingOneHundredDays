ff74020601716c203af58a49b69ef98687af8cf0
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2018-12-11 18:11:02 +0800

    httpxmlrequest

---
+++ netty/netty-demo/src/main/java/com/cherry/netty/demo/httpxml/HttpXmlRequestEncoder.java
@@ -0,0 +1,15 @@
+package com.cherry.netty.demo.httpxml;
+
+import java.util.List;
+
+import io.netty.channel.ChannelHandlerContext;
+
+public class HttpXmlRequestEncoder extends AbstractHttpXmlEncoder<HttpXmlRequest> {
+
+	@Override
+	protected void encode(ChannelHandlerContext arg0, HttpXmlRequest arg1, List<Object> arg2) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
