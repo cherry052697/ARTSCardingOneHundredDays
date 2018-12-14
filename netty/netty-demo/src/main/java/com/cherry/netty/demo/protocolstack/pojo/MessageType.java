3f05faf0eb0e4ec183d4386dc7f7fa1dc353a7cb
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2018-12-14 14:58:59 +0800

    MessageType

--- netty/netty-demo/src/main/java/com/cherry/netty/demo/protocolstack/pojo/MessageType.java
+++ netty/netty-demo/src/main/java/com/cherry/netty/demo/protocolstack/pojo/MessageType.java
@@ -1,7 +1,7 @@
 package com.cherry.netty.demo.protocolstack.pojo;
 
 public enum MessageType {
-	LOGIN_REQ((byte) 3), LOGIN_RESP((byte) 4), HEARTBEAT_REQ((byte) 5), HEARTBEAT_RESP((byte) 6),;
+	LOGIN_REQ((byte) 1), LOGIN_RESP((byte) 2), HEARTBEAT_REQ((byte) 3), HEARTBEAT_RESP((byte) 4),;
 
 	public byte value;
 
