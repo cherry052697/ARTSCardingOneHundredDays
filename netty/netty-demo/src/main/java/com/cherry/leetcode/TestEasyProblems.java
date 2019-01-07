66be09e23befb70c8a60bba8f7438585d7f95d84
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-01-07 18:19:15 +0800

    test

--- netty/netty-demo/src/main/java/com/cherry/leetcode/TestEasyProblems.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/TestEasyProblems.java
@@ -1,18 +1,32 @@
 package com.cherry.leetcode;
 
-import static org.assertj.core.api.Assertions.contentOf;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Stack;
 
+
+import java.util.List;
+
 public class TestEasyProblems {
 	public static void main(String[] args) {
 		TestEasyProblems test = new TestEasyProblems();
-		int x = 5;
-		System.out.println(test.climbStairs(x));
+		TreeNode root = new TreeNode(5);
+		TreeNode r1 = new TreeNode(1);
+		TreeNode l1 = new TreeNode(4);
+		TreeNode lr = new TreeNode(3);
+		TreeNode ll = new TreeNode(6);
+		root.left = r1;
+		root.right = l1;
+		l1.left = lr;
+		l1.right = ll;
+		System.out.println(test.isValidBST(root));
+		
+		
+
 	}
 
 	/*
@@ -20,41 +34,156 @@ public class TestEasyProblems {
 	 * five terms as following: 1. 1 2. 11 3. 21 4. 1211 5. 111221
 	 */
 	public String countAndSay(int n) {
-
 		return null;
+	}
+	
+	
+	/*
+	 * Given a binary tree, determine if it is a valid binary search tree (BST).
+		Assume a BST is defined as follows:
+		The left subtree of a node contains only nodes with keys less than the node's key.
+		The right subtree of a node contains only nodes with keys greater than the node's key.
+		Both the left and right subtrees must also be binary search trees.
+	 */
+	public boolean isValidBST(TreeNode root) {
+		TreeNode pre = null;
+		 // Traverse the tree in inorder.
+        if (root != null) {
+            // Inorder traversal: left first.
+            if (!isValidBST(root.left)) return false;
+            
+            // Compare it with the previous value in inorder traversal.
+            if (pre != null && root.val <= pre.val) return false;
+            
+            // Update the previous value.
+            pre = root;
+            
+            // Inorder traversal: right last.
+            return isValidBST(root.right);
+        }
+        return true;
+    }
 
+	/*
+	 * Given an array nums of n integers, are there elements a, b, c in nums
+	 * such that a + b + c = 0? Find all unique triplets in the array which
+	 * gives the sum of zero.
+	 */
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		Arrays.sort(nums);
+		for (int i = 0; i <= nums.length - 3; i++)
+			if (nums[i] <= 0) {
+				if (i >= 1 && nums[i] == nums[i - 1])
+					continue;
+				int j = i + 1;
+				int k = nums.length - 1;
+				while (j < k) {
+					if (nums[i] + nums[j] + nums[k] == 0) {
+						List<Integer> list = new ArrayList<>();
+						list.add(nums[i]);
+						list.add(nums[j]);
+						list.add(nums[k]);
+						result.add(list);
+						j++;
+						k--;
+						while (j < k && nums[j] == nums[j - 1])
+							j++;
+						while (j < k && nums[k] == nums[k + 1])
+							k--;
+					}else {
+						boolean flag = true;
+						while (flag) {
+							while (j < k && nums[j] + nums[k] > (-1) * nums[i])
+								k--;
+							while (j < k && nums[j] + nums[k] < (-1) * nums[i])
+								j++;
+							if (j >= k || nums[j] + nums[k] == (-1) * nums[i])
+								flag = false;
+						}
+					}
+				} 
+			}
+		return result;
 	}
 
 	/*
-	 * very important
-	 * You are climbing a stair case. It takes n steps to reach to the top. Each
-	 * time you can either climb 1 or 2 steps. In how many distinct ways can you
-	 * climb to the top? Note: Given n will be a positive integer.
+	 * every important Given an integer array, you need to find one continuous
+	 * subarray that if you only sort this subarray in ascending order, then the
+	 * whole array will be sorted in ascending order, too.
+	 */
+
+	public int findUnsortedSubarray(int[] nums) {
+		int n = nums.length;
+		int beginInt = -1;
+		int endInt = -2;
+
+		int min = nums[n - 1];
+		int max = nums[0];
+
+		for (int i = 0; i < n; i++) {
+			max = Math.max(max, nums[i]);
+			min = Math.min(min, nums[n - 1 - i]);
+			if (nums[i] < max)
+				endInt = i;
+			if (nums[n - 1 - i] > min)
+				beginInt = n - 1 - i;
+			System.out.println("i=" + i + ",max=" + max + ",min=" + min + ",end=" + endInt + ",beg=" + beginInt);
+		}
+		return endInt - beginInt + 1;
+	}
+
+	/*
+	 * very important Given two sorted integer arrays nums1 and nums2, merge
+	 * nums2 into nums1 as one sorted array
+	 */
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		m = m - 1;
+		n = n - 1;
+		int i = m + n + 1;
+		while (m >= 0 || n >= 0) {
+			if (m < 0) {
+				nums1[i--] = nums2[n--];
+			} else if (n < 0) {
+				nums1[i--] = nums1[m--];
+			} else {
+				nums1[i--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+			}
+		}
+	}
+
+	/*
+	 * very important You are climbing a stair case. It takes n steps to reach
+	 * to the top. Each time you can either climb 1 or 2 steps. In how many
+	 * distinct ways can you climb to the top? Note: Given n will be a positive
+	 * integer.
 	 */
 	public int climbStairs2(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
-    }
+		if (n == 1) {
+			return 1;
+		}
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		return dp[n];
+	}
+
 	public int climbStairs(int n) {
-        return climb_Stairs(0, n);
-    }
-    public int climb_Stairs(int i, int n) {
-        if (i > n) {
-            return 0;
-        }
-        if (i == n) {
-            return 1;
-        }
-        return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
-    }
+		return climb_Stairs(0, n);
+	}
+
+	public int climb_Stairs(int i, int n) {
+		if (i > n) {
+			return 0;
+		}
+		if (i == n) {
+			return 1;
+		}
+		return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+	}
 
 	/*
 	 * Compute and return the square root of x, where x is guaranteed to be a
