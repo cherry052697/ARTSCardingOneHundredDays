19a88b2f6d823b563f69b839c602e3a362f8ac2f
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-01-10 18:20:28 +0800

    targetSum

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -727,9 +727,35 @@ public class Top100LikedQuestions {
 	 * The sum of elements in the given array will not exceed 1000.
 	 * Your output answer is guaranteed to be fitted in a 32-bit integer.
 	 */
-	public int findTargetSumWays(int[] nums, int S) {
-		
-		return 0;
-	}
+	int count = 0;
+    public int findTargetSumWays(int[] nums, int S) {
+        calculate(nums, 0, 0, S);
+        return count;
+    }
+    public void calculate(int[] nums, int i, int sum, int S) {
+        if (i == nums.length) {
+            if (sum == S)
+                count++;
+        } else {
+            calculate(nums, i + 1, sum + nums[i], S);
+            calculate(nums, i + 1, sum - nums[i], S);
+        }
+    }
+    
+    public int findTargetSumWays2(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1); 
+    }   
+
+    public int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1]; 
+        dp[0] = 1;
+        for (int n : nums)
+            for (int i = s; i >= n; i--)
+                dp[i] += dp[i - n]; 
+        return dp[s];
+    } 
 
 }
