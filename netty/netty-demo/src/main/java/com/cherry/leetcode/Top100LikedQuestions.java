8d35244d4f9ddc0b92036d9449630f597914b0dc
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-01-14 09:59:41 +0800

    findMedianSortedArrays

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -1,6 +1,5 @@
 package com.cherry.leetcode;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -230,7 +229,6 @@ public class Top100LikedQuestions {
 		}
 		return ans;
 	}
-
 
 	/*
 	 * 617. Merge Two Binary Trees Given two binary trees and imagine that when
@@ -1458,4 +1456,69 @@ public class Top100LikedQuestions {
 	public int countSubstrings(String s) {
 		return 1;
 	}
+
+	/*
+	 * 4. Median of Two Sorted Arrays
+	 * 
+	 * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+	 * Find the median of the two sorted arrays. The overall run time complexity
+	 * should be O(log (m+n)). You may assume nums1 and nums2 cannot be both
+	 * empty.
+	 */
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int m = nums1.length, n = nums2.length, left = (m + n + 1) / 2, right = (m + n + 2) / 2;
+		return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0;
+	}
+
+	int findKth(int[] nums1, int i, int[] nums2, int j, int k) {
+		if (i >= nums1.length)
+			return nums2[j + k - 1];
+		if (j >= nums2.length)
+			return nums1[i + k - 1];
+		if (k == 1)
+			return Math.min(nums1[i], nums2[j]);
+		int midVal1 = (i + k / 2 - 1 < nums1.length) ? nums1[i + k / 2 - 1] : Integer.MAX_VALUE;
+		int midVal2 = (j + k / 2 - 1 < nums2.length) ? nums2[j + k / 2 - 1] : Integer.MAX_VALUE;
+		if (midVal1 < midVal2) {
+			return findKth(nums1, i + k / 2, nums2, j, k - k / 2);
+		} else {
+			return findKth(nums1, i, nums2, j + k / 2, k - k / 2);
+		}
+	}
+	
+	public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length, left = (m + n + 1) / 2, right = (m + n + 2) / 2;
+        return (findKth2(nums1, nums2, left) + findKth2(nums1, nums2, right)) / 2.0;
+    }
+    int findKth2(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length, n = nums2.length;
+        if (m == 0) return nums2[k - 1];
+        if (n == 0) return nums1[k - 1];
+        if (k == 1) return Math.min(nums1[0], nums2[0]);
+        int i = Math.min(m, k / 2), j = Math.min(n, k / 2);
+        if (nums1[i - 1] > nums2[j - 1]) {
+            return findKth2(nums1, Arrays.copyOfRange(nums2, j, n), k - j);
+        } else {
+            return findKth2(Arrays.copyOfRange(nums1, i, m), nums2, k - i);
+        }
+    }
+    public double findMedianSortedArrays3(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+		if (m < n)
+			return findMedianSortedArrays(nums2, nums1);
+        if (n == 0) return (nums1[(m - 1) / 2] + nums1[m / 2]) / 2.0;
+        int left = 0, right = 2 * n;
+        while (left <= right) {
+            int mid2 = (left + right) / 2;
+            int mid1 = m + n - mid2;
+            double L1 = mid1 == 0 ? Double.MIN_VALUE : nums1[(mid1 - 1) / 2];
+            double L2 = mid2 == 0 ? Double.MIN_VALUE : nums2[(mid2 - 1) / 2];
+            double R1 = mid1 == m * 2 ? Double.MAX_VALUE : nums1[mid1 / 2];
+            double R2 = mid2 == n * 2 ? Double.MAX_VALUE : nums2[mid2 / 2];
+            if (L1 > R2) left = mid2 + 1;
+            else if (L2 > R1) right = mid2 - 1;
+            else return (Math.max(L1, L2) + Math.min(R1, R2)) / 2;
+        }
+        return -1;
+    }
 }
