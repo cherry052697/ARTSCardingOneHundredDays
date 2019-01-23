7a6adbdade6ca352713ac0f5dad5304ffc6ce6c8
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-01-23 16:04:57 +0800

    java-senior

---
+++ netty/netty-demo/src/main/java/com/cherry/senior/Multithreading.java
@@ -0,0 +1,9 @@
+package com.cherry.senior;
+
+public class Multithreading {
+	public static void main(String[] args) {
+		ThreadLocal local = new ThreadLocal<>();
+		String s = new String();
+	}
+
+}
