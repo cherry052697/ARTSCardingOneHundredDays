ae752c433362c5a087dcdd9ceba1501c34c2f223
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-19 17:47:29 +0800

    300. Longest Increasing Subsequence

--- netty/netty-demo/src/main/java/com/cherry/leetcode/DynamicProgramming.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/DynamicProgramming.java
@@ -364,4 +364,31 @@ public class DynamicProgramming {
 
 	}
 
+	public int lcs(String str1, String str2) {
+		int len1 = str1.length();
+		int len2 = str2.length();
+		int c[][] = new int[len1 + 1][len2 + 1];
+		for (int i = 0; i <= len1; i++) {
+			for (int j = 0; j <= len2; j++) {
+				if (i == 0 || j == 0) {
+					c[i][j] = 0;
+				} else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+					c[i][j] = c[i - 1][j - 1] + 1;
+				} else {
+					c[i][j] = Math.max(c[i - 1][j], c[i][j - 1]);
+				}
+			}
+		}
+		return c[len1][len2];
+	}
+
+	/*
+	 * 300. Longest Increasing Subsequence
+	 * 
+	 * Given an unsorted array of integers, find the length of longest increasing subsequence.
+	 */
+	public int lengthOfLIS(int[] nums) {
+		return 0;
+	}
+
 }
