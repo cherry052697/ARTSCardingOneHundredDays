a1abef93e3e941c1cdab0172bdd256efb979a4df
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-19 15:57:38 +0800

    337. House Robber III

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -2833,5 +2833,24 @@ public class Top100LikedQuestions {
         levelHelper(root.left, height+1,res);
         levelHelper(root.right, height+1,res);
     }
-
+    /*
+     * 337. House Robber III
+     * 
+     * The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+     * Determine the maximum amount of money the thief can rob tonight without alerting the police.
+     */
+    public int rob(TreeNode root) {
+		int[] num = dfs(root);
+        return Math.max(num[0], num[1]);
+    }
+    private int[] dfs(TreeNode x) {
+        if (x == null) return new int[2];
+        int[] left = dfs(x.left);
+        int[] right = dfs(x.right);
+        int[] res = new int[2];
+        res[0] = left[1] + right[1] + x.val;
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return res;
+    }
+    
 }
