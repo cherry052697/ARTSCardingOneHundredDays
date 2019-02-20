f29d2c03b7eaebe057657910f7678e165350d219
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-20 12:06:24 +0800

    343. Integer Break

--- netty/netty-demo/src/main/java/com/cherry/leetcode/DynamicProgramming.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/DynamicProgramming.java
@@ -18,7 +18,8 @@ public class DynamicProgramming {
 		// System.out.println(dp.minimumDeleteSum2("delete", "leet"));
 
 		int[] nums = { 10, 9, 2, 5, 3, 7, 101, 18 };
-		System.out.println(dp.lengthOfLIS(nums));
+		// System.out.println(dp.lengthOfLIS(nums));
+		System.out.println(dp.integerBreak(10));
 
 	}
 
@@ -476,6 +477,39 @@ public class DynamicProgramming {
 				ans++;
 			}
 		return ans;
+	}
+
+	/*
+	 * 343. Integer Break
+	 * 
+	 * Given a positive integer n, break it into the sum of at least two
+	 * positive integers and maximize the product of those integers. Return the
+	 * maximum product you can get
+	 */
+	public int integerBreak(int n) {
+		if (n == 2)
+			return 1;
+		if (n == 3)
+			return 2;
+		int product = 1;
+		while (n > 4) {
+			product *= 3;
+			n -= 3;
+		}
+		product *= n;
+
+		return product;
+	}
+
+	public int integerBreak2(int n) {
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j < i; j++) {
+				dp[i] = Math.max(dp[i], (Math.max(j, dp[j])) * (Math.max(i - j, dp[i - j])));
+			}
+		}
+		return dp[n];
 	}
 
 }
