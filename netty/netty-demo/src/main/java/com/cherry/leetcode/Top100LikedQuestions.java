63b8a1ad9f7246592488046b9597337adea5fdc6
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-20 17:48:15 +0800

    200. Number of Islands

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -3061,4 +3061,87 @@ public class Top100LikedQuestions {
 		}
 		return record[n];
 	}
+
+	/*
+	 * 200. Number of Islands
+	 * 
+	 * Given a 2d grid map of '1's (land) and '0's (water), count the number of
+	 * islands. An island is surrounded by water and is formed by connecting
+	 * adjacent lands horizontally or vertically. You may assume all four edges
+	 * of the grid are all surrounded by water.
+	 */
+	int n, m;
+
+	public int numIslands(char[][] grid) {
+		int count = 0;
+		n = grid.length;
+		if (n == 0)
+			return 0;
+		m = grid[0].length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++)
+				if (grid[i][j] == '1') {
+					DFSMarking(grid, i, j);
+					++count;
+				}
+		}
+		return count;
+	}
+
+	private void DFSMarking(char[][] grid, int i, int j) {
+		if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1')
+			return;
+		grid[i][j] = '0';
+		DFSMarking(grid, i + 1, j);
+		DFSMarking(grid, i - 1, j);
+		DFSMarking(grid, i, j + 1);
+		DFSMarking(grid, i, j - 1);
+	}
+
+	public int numIslands2(char[][] grid) {
+		int count = 0;
+		for (int i = 0; i < grid.length; i++)
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == '1') {
+					bfsFill(grid, i, j);
+					count++;
+				}
+			}
+		return count;
+	}
+
+	private void bfsFill(char[][] grid, int x, int y) {
+		grid[x][y] = '0';
+		int n = grid.length;
+		int m = grid[0].length;
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		int code = x * m + y;
+		queue.offer(code);
+		while (!queue.isEmpty()) {
+			code = queue.poll();
+			int i = code / m;
+			int j = code % m;
+			if (i > 0 && grid[i - 1][j] == '1') // search upward and mark
+												// adjacent '1's as '0'.
+			{
+				queue.offer((i - 1) * m + j);
+				grid[i - 1][j] = '0';
+			}
+			if (i < n - 1 && grid[i + 1][j] == '1') // down
+			{
+				queue.offer((i + 1) * m + j);
+				grid[i + 1][j] = '0';
+			}
+			if (j > 0 && grid[i][j - 1] == '1') // left
+			{
+				queue.offer(i * m + j - 1);
+				grid[i][j - 1] = '0';
+			}
+			if (j < m - 1 && grid[i][j + 1] == '1') // right
+			{
+				queue.offer(i * m + j + 1);
+				grid[i][j + 1] = '0';
+			}
+		}
+	}
 }
