2213f00d5f135ccbc8cac8f6b4b4bcf6d4b9a157
cherry052697 <Bruce@BruceWang-pc>
2019-02-23 19:37:54 +0800

    669. Trim a Binary Search Tree

--- netty/netty-demo/src/main/java/com/cherry/leetcode/TreeApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/TreeApplication.java
@@ -106,6 +106,27 @@ public class TreeApplication {
 			node.right = null;
 		return node.val == 1 || a1 || a2;
 	}
+	/*
+	 * 669. Trim a Binary Search Tree
+	 * 
+	 * Given a binary search tree and the lowest and highest boundaries as L and
+	 * R, trim the tree so that all its elements lies in [L, R] (R >= L). You
+	 * might need to change the root of the tree, so the result should return
+	 * the new root of the trimmed binary search tree.
+	 */
+
+	public TreeNode trimBST(TreeNode root, int L, int R) {
+		if (root == null)
+			return root;
+		if (root.val > R)
+			return trimBST(root.left, L, R);
+		if (root.val < L)
+			return trimBST(root.right, L, R);
+
+		root.left = trimBST(root.left, L, R);
+		root.right = trimBST(root.right, L, R);
+		return root;
+	}
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
