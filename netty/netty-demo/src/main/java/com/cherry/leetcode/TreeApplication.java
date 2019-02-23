ace1df2fbbca72d4fcddb35d20a9c00ea67c3d6a
cherry052697 <Bruce@BruceWang-pc>
2019-02-23 19:23:15 +0800

    814. Binary Tree Pruning

--- netty/netty-demo/src/main/java/com/cherry/leetcode/TreeApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/TreeApplication.java
@@ -17,59 +17,107 @@ public class TreeApplication {
 	 * given array and output the root node of this tree.
 	 */
 	public TreeNode constructMaximumBinaryTree(int[] nums) {
-		 Deque<TreeNode> stack = new LinkedList<>();
-	        for(int i = 0; i < nums.length; i++) {
-	            TreeNode curr = new TreeNode(nums[i]);
-	            while(!stack.isEmpty() && stack.peek().val < nums[i]) {
-	                curr.left = stack.pop();
-	            }
-	            if(!stack.isEmpty()) {
-	                stack.peek().right = curr;
-	            }
-	            stack.push(curr);
-	        }
-	        
-	        return stack.isEmpty() ? null : stack.removeLast();
+		Deque<TreeNode> stack = new LinkedList<>();
+		for (int i = 0; i < nums.length; i++) {
+			TreeNode curr = new TreeNode(nums[i]);
+			while (!stack.isEmpty() && stack.peek().val < nums[i]) {
+				curr.left = stack.pop();
+			}
+			if (!stack.isEmpty()) {
+				stack.peek().right = curr;
+			}
+			stack.push(curr);
+		}
+
+		return stack.isEmpty() ? null : stack.removeLast();
 	}
-	 public TreeNode constructMaximumBinaryTree2(int[] nums) {
-	        if (nums == null) return null;
-	        return build(nums, 0, nums.length - 1);
-	    }
-	    
-	    private TreeNode build(int[] nums, int start, int end) {
-	        if (start > end) return null;
-	        
-	        int idxMax = start;
-	        for (int i = start + 1; i <= end; i++) {
-	            if (nums[i] > nums[idxMax]) {
-	                idxMax = i;
-	            }
-	        }
-	        
-	        TreeNode root = new TreeNode(nums[idxMax]);
-	        
-	        root.left = build(nums, start, idxMax - 1);
-	        root.right = build(nums, idxMax + 1, end);
-	        
-	        return root;
-	    }
-	
-	int treeNodeSize(TreeNode node){
+
+	public TreeNode constructMaximumBinaryTree2(int[] nums) {
+		if (nums == null)
+			return null;
+		return build(nums, 0, nums.length - 1);
+	}
+
+	private TreeNode build(int[] nums, int start, int end) {
+		if (start > end)
+			return null;
+
+		int idxMax = start;
+		for (int i = start + 1; i <= end; i++) {
+			if (nums[i] > nums[idxMax]) {
+				idxMax = i;
+			}
+		}
+
+		TreeNode root = new TreeNode(nums[idxMax]);
+
+		root.left = build(nums, start, idxMax - 1);
+		root.right = build(nums, idxMax + 1, end);
+
+		return root;
+	}
+
+	int treeNodeSize(TreeNode node) {
 		TreeNode leftnode = node.left;
 		TreeNode rightnode = node.right;
 		int size = 1;
-		if (leftnode != null) 
+		if (leftnode != null)
 			size += treeNodeSize(leftnode);
-		if(rightnode != null)
+		if (rightnode != null)
 			size += treeNodeSize(rightnode);
 		return size;
-		
+
 	}
 
+	TreeNode inorderTreeNode(TreeNode root) {
+		if (root == null)
+			return null;
+		inorderTreeNode(root.left);
+		inorderTreeNode(root.right);
+		return root;
+	}
+
+	/*
+	 * 814. Binary Tree Pruning
+	 * 
+	 * We are given the head node root of a binary tree, where additionally
+	 * every node's value is either a 0 or a 1.
+	 * 
+	 * Return the same tree where every subtree (of the given tree) not
+	 * containing a 1 has been removed.
+	 * 
+	 * (Recall that the subtree of a node X is X, plus every node that is a
+	 * descendant of X.)
+	 * 
+	 */
+
+	public TreeNode pruneTree(TreeNode root) {
+		return containsOne(root) ? root : null;
+	}
+
+	public boolean containsOne(TreeNode node) {
+		if (node == null)
+			return false;
+		boolean a1 = containsOne(node.left);
+		boolean a2 = containsOne(node.right);
+		if (!a1)
+			node.left = null;
+		if (!a2)
+			node.right = null;
+		return node.val == 1 || a1 || a2;
+	}
+
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		TreeApplication ta = new TreeApplication();
-		int[] nums = {3,2,1,6,0,5};
-		System.out.println(ta.constructMaximumBinaryTree(nums));
+		int[] nums = { 3, 2, 1, 6, 0, 5 };
+		// System.out.println(ta.constructMaximumBinaryTree(nums));
+
+		TreeNode node = new TreeNode(1);
+		node.right = new TreeNode(0);
+		node.right.left = new TreeNode(0);
+		node.right.right = new TreeNode(1);
+		System.out.println(ta.pruneTree(node));
 	}
 
 }
