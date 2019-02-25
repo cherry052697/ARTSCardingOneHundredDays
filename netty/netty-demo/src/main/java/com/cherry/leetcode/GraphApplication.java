3ea486baa6785524a8020ca0b999ac5b689aa620
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-25 16:51:15 +0800

    684. Redundant Connection

--- netty/netty-demo/src/main/java/com/cherry/leetcode/GraphApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/GraphApplication.java
@@ -1,6 +1,9 @@
 package com.cherry.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Stack;
 
 public class GraphApplication {
@@ -69,6 +72,7 @@ public class GraphApplication {
 	 * 
 	 * You can walk back and forth between rooms freely.
 	 * 
+	 * 
 	 * Return true if and only if you can enter every room.
 	 */
 	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
@@ -88,6 +92,60 @@ public class GraphApplication {
 			if (!v)
 				return false;
 		return true;
+	}
+
+	/*
+	 * 684. Redundant Connection
+	 * 
+	 * In this problem, a tree is an undirected graph that is connected and has
+	 * no cycles.
+	 * 
+	 * The given input is a graph that started as a tree with N nodes (with
+	 * distinct values 1, 2, ..., N), with one additional edge added. The added
+	 * edge has two different vertices chosen from 1 to N, and was not an edge
+	 * that already existed.
+	 * 
+	 * The resulting graph is given as a 2D-array of edges. Each element of
+	 * edges is a pair [u, v] with u < v, that represents an undirected edge
+	 * connecting nodes u and v.
+	 * 
+	 * Return an edge that can be removed so that the resulting graph is a tree
+	 * of N nodes. If there are multiple answers, return the answer that occurs
+	 * last in the given 2D-array. The answer edge [u, v] should be in the same
+	 * format, with u < v.
+	 */
+	Set<Integer> seen = new HashSet<Integer>();
+	int MAX_EDGE_VAL = 1000;
+
+	@SuppressWarnings("unchecked")
+	public int[] findRedundantConnection(int[][] edges) {
+		ArrayList<Integer>[] graph = new ArrayList[MAX_EDGE_VAL + 1];
+		for (int i = 0; i <= MAX_EDGE_VAL; i++) {
+			graph[i] = new ArrayList();
+		}
+
+		for (int[] edge : edges) {
+			seen.clear();
+			if (!graph[edge[0]].isEmpty() && !graph[edge[1]].isEmpty() && dfs(graph, edge[0], edge[1])) {
+				return edge;
+			}
+			graph[edge[0]].add(edge[1]);
+			graph[edge[1]].add(edge[0]);
+		}
+		throw new AssertionError();
+	}
+
+	public boolean dfs(ArrayList<Integer>[] graph, int source, int target) {
+		if (!seen.contains(source)) {
+			seen.add(source);
+			if (source == target)
+				return true;
+			for (int nei : graph[source]) {
+				if (dfs(graph, nei, target))
+					return true;
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
