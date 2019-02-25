2460f3743c84e8d51f177fc20001b9089b46b7a0
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-25 15:44:19 +0800

    997. Find the Town Judge

--- netty/netty-demo/src/main/java/com/cherry/leetcode/GraphApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/GraphApplication.java
@@ -19,6 +19,34 @@ public class GraphApplication {
 		return 0;
 	}
 
+	/*
+	 * 997. Find the Town Judge
+	 * 
+	 * In a town, there are N people labelled from 1 to N. There is a rumor that
+	 * one of these people is secretly the town judge.
+	 * 
+	 * If the town judge exists, then:
+	 * 
+	 * The town judge trusts nobody. Everybody (except for the town judge)
+	 * trusts the town judge. There is exactly one person that satisfies
+	 * properties 1 and 2. You are given trust, an array of pairs trust[i] = [a,
+	 * b] representing that the person labelled a trusts the person labelled b.
+	 * 
+	 * If the town judge exists and can be identified, return the label of the
+	 * town judge. Otherwise, return -1.
+	 */
+	public int findJudge(int N, int[][] trust) {
+		int[] count = new int[N+1];
+        for (int[] t: trust) {
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
+        }
+        return -1;
+	}
+
 	public static void main(String[] args) {
 
 	}
