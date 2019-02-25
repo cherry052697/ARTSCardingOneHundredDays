7a21914dcc0313260233f002f4058237634eca22
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-25 16:07:32 +0800

    841. Keys and Rooms

--- netty/netty-demo/src/main/java/com/cherry/leetcode/GraphApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/GraphApplication.java
@@ -1,5 +1,8 @@
 package com.cherry.leetcode;
 
+import java.util.List;
+import java.util.Stack;
+
 public class GraphApplication {
 
 	/*
@@ -34,21 +37,63 @@ public class GraphApplication {
 	 * 
 	 * If the town judge exists and can be identified, return the label of the
 	 * town judge. Otherwise, return -1.
+	 * 
+	 * Intuition: Consider trust as a graph, all pairs are directed edge. The
+	 * point with in-degree - out-degree = N - 1 become the judge.
 	 */
 	public int findJudge(int N, int[][] trust) {
-		int[] count = new int[N+1];
-        for (int[] t: trust) {
-            count[t[0]]--;
-            count[t[1]]++;
-        }
-        for (int i = 1; i <= N; ++i) {
-            if (count[i] == N - 1) return i;
-        }
-        return -1;
+		int[] count = new int[N + 1];
+		for (int[] t : trust) {
+			count[t[0]]--;
+			count[t[1]]++;
+		}
+		for (int i = 1; i <= N; ++i) {
+			if (count[i] == N - 1)
+				return i;
+		}
+		return -1;
+	}
+
+	/*
+	 * 841. Keys and Rooms
+	 * 
+	 * There are N rooms and you start in room 0. Each room has a distinct
+	 * number in 0, 1, 2, ..., N-1, and each room may have some keys to access
+	 * the next room.
+	 * 
+	 * Formally, each room i has a list of keys rooms[i], and each key
+	 * rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length. A
+	 * key rooms[i][j] = v opens the room with number v.
+	 * 
+	 * Initially, all the rooms start locked (except for room 0).
+	 * 
+	 * You can walk back and forth between rooms freely.
+	 * 
+	 * Return true if and only if you can enter every room.
+	 */
+	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+		boolean[] seen = new boolean[rooms.size()];
+		seen[0] = true;
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(0);
+		while (!stack.isEmpty()) {
+			int node = stack.pop();
+			for (int nei : rooms.get(node))
+				if (!seen[nei]) {
+					seen[nei] = true;
+					stack.push(nei);
+				}
+		}
+		for (boolean v : seen)
+			if (!v)
+				return false;
+		return true;
 	}
 
 	public static void main(String[] args) {
-
+		GraphApplication ga = new GraphApplication();
+		int[][] trust = { { 1, 3 }, { 1, 4 }, { 2, 3 }, { 2, 4 }, { 4, 3 } };
+		System.out.println(ga.findJudge(4, trust));
 	}
 
 }
