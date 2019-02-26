131c1f1b5db8688fd3bb516a219f3cd5e4f3618d
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-26 17:45:42 +0800

    34. Find First and Last Position of Element in Sorted Array

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -3201,7 +3201,13 @@ public class Top100LikedQuestions {
 	 * If the target is not found in the array, return [-1, -1].
 	 */
 	public int[] searchRange(int[] nums, int target) {
-		
-		return null;
+		int[] result = {-1,-1};
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == target ) {
+				result[0]=result[0]==-1?i:result[0];
+				result[1]=result[1]==-1?i:(result[1]<i?i:result[1]);
+			}
+		}
+		return result;
 	}
 }
