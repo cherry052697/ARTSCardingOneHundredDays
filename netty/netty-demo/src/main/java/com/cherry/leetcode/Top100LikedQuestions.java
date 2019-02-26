48e6d6c5e83e7438f8296c02b21214e08c4b46c6
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-02-26 17:30:49 +0800

    34. Find First and Last Position of Element in Sorted Array

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -3189,4 +3189,19 @@ public class Top100LikedQuestions {
 	public int maxCoins(int[] nums) {
 		return 0;
 	}
+
+	/*
+	 * 34. Find First and Last Position of Element in Sorted Array
+	 * 
+	 * Given an array of integers nums sorted in ascending order, find the
+	 * starting and ending position of a given target value.
+	 * 
+	 * Your algorithm's runtime complexity must be in the order of O(log n).
+	 * 
+	 * If the target is not found in the array, return [-1, -1].
+	 */
+	public int[] searchRange(int[] nums, int target) {
+		
+		return null;
+	}
 }
