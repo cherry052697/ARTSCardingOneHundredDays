e2f71249c41efcd103cf322a70419a6d555dfd27
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-03-01 16:30:30 +0800

    rangeSumBST1

--- netty/netty-demo/src/main/java/com/cherry/leetcode/BinarySearchTreeApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/BinarySearchTreeApplication.java
@@ -86,6 +86,12 @@ public class BinarySearchTreeApplication {
 		return sum;
 	}
 
+	public int rangeSumBST1(TreeNode root, int L, int R) {
+        if(root == null) return 0;
+        if(root.val > R) return rangeSumBST(root.left, L, R);
+        if(root.val < L) return rangeSumBST(root.right, L, R);
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);      
+    }
 
 	public static void main(String[] args) {
 		BinarySearchTreeApplication asta = new BinarySearchTreeApplication();
