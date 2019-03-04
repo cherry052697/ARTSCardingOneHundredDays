06f79fc780c1fc25019e2bb7865cb9a4bd88bffb
cherry052697 <Bruce@windows10.microdone.cn>
2019-03-04 22:22:31 +0800

    922. Sort Array By Parity II

--- netty/netty-demo/src/main/java/com/cherry/leetcode/SortAlgorithmsApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/SortAlgorithmsApplication.java
@@ -53,6 +53,53 @@ public class SortAlgorithmsApplication {
 		return ans;
 	}
 
+	/*
+	 * 922. Sort Array By Parity II
+	 * 
+	 * Given an array A of non-negative integers, half of the integers in A are
+	 * odd, and half of the integers are even.
+	 * 
+	 * Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i]
+	 * is even, i is even.
+	 * 
+	 * You may return any answer array that satisfies this condition.
+	 */
+	public int[] sortArrayByParityII(int[] A) {
+		int N = A.length;
+		int[] ans = new int[N];
+
+		int t = 0;
+		for (int x : A)
+			if (x % 2 == 0) {
+				ans[t] = x;
+				t += 2;
+			}
+
+		t = 1;
+		for (int x : A)
+			if (x % 2 == 1) {
+				ans[t] = x;
+				t += 2;
+			}
+
+		return ans;
+	}
+
+	public int[] sortArrayByParityII2(int[] A) {
+		int j = 1;
+		for (int i = 0; i < A.length; i += 2)
+			if (A[i] % 2 == 1) {
+				while (A[j] % 2 == 1)
+					j += 2;
+
+				int tmp = A[i];
+				A[i] = A[j];
+				A[j] = tmp;
+			}
+
+		return A;
+	}
+
 	public static void main(String[] args) {
 		SortAlgorithmsApplication saa = new SortAlgorithmsApplication();
 		int[] a = { -4, -1, 0, 3, 10 };
