0d38c7a43e7c394331138c9f5557eb279018edc8
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-03-05 15:19:16 +0800

    139. Word Break

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -3432,4 +3432,21 @@ public class Top100LikedQuestions {
 		return null;
 	}
 
+	/*
+	 * 139. Word Break
+	 * 
+	 * Given a non-empty string s and a dictionary wordDict containing a list of
+	 * non-empty words, determine if s can be segmented into a space-separated
+	 * sequence of one or more dictionary words.
+	 * 
+	 * Note:
+	 * 
+	 * The same word in the dictionary may be reused multiple times in the
+	 * segmentation. You may assume the dictionary does not contain duplicate
+	 * words.
+	 */
+	public boolean wordBreak(String s, List<String> wordDict) {
+		return false;
+	}
+
 }
