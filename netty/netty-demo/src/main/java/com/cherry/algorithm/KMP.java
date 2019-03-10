cb99a86f8ce124959baa456b967a09dcfcb8cc1b
cherry052697 <Bruce@windows10.microdone.cn>
2019-03-10 18:37:36 +0800

    kmpMatcher

--- netty/netty-demo/src/main/java/com/cherry/algorithm/KMP.java
+++ netty/netty-demo/src/main/java/com/cherry/algorithm/KMP.java
@@ -8,17 +8,36 @@ public class KMP {
 		int[] next = new int[len];
 		int t = next[0] = -1;
 		while (j < len - 1) {
+//			System.out.print("j="+j+",t="+t);
 			if (0 > t || p[j] == p[t])
 				next[++j] = ++t;
 			else
 				t = next[t];
+//			System.out.println(",after  j="+j+",t="+t+",next["+j+"]="+next[j]);
 		}
 		return next;
 	}
 	
+	int kmpMatcher(String t,String p){
+		int n = t.length(),i=0,j=0,m=p.length();
+		int[] next = buildNext(p.toCharArray());
+		while(i < n && j< m){
+			if(j==-1||t.charAt(i)==p.charAt(j)){
+				i++;j++;
+			}else{
+				j=next[j];
+			}
+		}
+		if (j == m) 
+			return i-j;
+		else
+			return -1;
+			
+	}
 	public static void main(String[] args) {
 		KMP kmp = new KMP();
-		System.out.println(JsonUtil.toJson(kmp.buildNext("abcabcdabc".toCharArray())));
+		System.out.println(JsonUtil.toJson(kmp.buildNext("ababaca".toCharArray())));
+		System.out.println(kmp.kmpMatcher("abcabcababadabcbdf", "abab"));
 	}
 
 }
