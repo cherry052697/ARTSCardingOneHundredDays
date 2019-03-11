d2bf7969eeb72b07c68cfebe8ee8762e10ab5614
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-03-11 10:39:22 +0800

    42. Trapping Rain Water

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -3630,7 +3630,25 @@ public class Top100LikedQuestions {
 	 * raining.
 	 */
 	public int trap(int[] height) {
-		return 0;
+		int left = 0, right = height.length - 1;
+		int ans = 0;
+		int left_max = 0, right_max = 0;
+		while (left < right) {
+			if (height[left] < height[right]) {
+				if (height[left] >= left_max)
+					left_max = height[left];
+				else
+					ans += (left_max - height[left]);
+				++left;
+			} else {
+				if (height[right] >= right_max)
+					right_max = height[right];
+				else
+					ans += (right_max - height[right]);
+				--right;
+			}
+		}
+		return ans;
 	}
 
 }
