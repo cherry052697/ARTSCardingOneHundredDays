b367acbd530b4134781f7ac8c02ee727aa44250f
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-03-12 14:20:02 +0800

    124. Binary Tree Maximum Path Sum

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -3746,4 +3746,31 @@ public class Top100LikedQuestions {
 		return r;
 	}
 
+	/*
+	 * 124. Binary Tree Maximum Path Sum
+	 * 
+	 * Given a non-empty binary tree, find the maximum path sum.
+	 * 
+	 * For this problem, a path is defined as any sequence of nodes from some
+	 * starting node to any node in the tree along the parent-child connections.
+	 * The path must contain at least one node and does not need to go through
+	 * the root.
+	 */
+	int maxValue;
+
+	public int maxPathSum(TreeNode root) {
+		maxValue = Integer.MIN_VALUE;
+		maxPathDown(root);
+		return maxValue;
+	}
+
+	private int maxPathDown(TreeNode node) {
+		if (node == null)
+			return 0;
+		int left = Math.max(0, maxPathDown(node.left));
+		int right = Math.max(0, maxPathDown(node.right));
+		maxValue = Math.max(maxValue, left + right + node.val);
+		return Math.max(left, right) + node.val;
+	}
+
 }
