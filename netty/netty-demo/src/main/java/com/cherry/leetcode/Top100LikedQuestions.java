d5ba62c82f45ba8d69eeb55f9e6fdbf3fa8404b9
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-03-13 17:32:24 +0800

    128. Longest Consecutive Sequence

--- netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/Top100LikedQuestions.java
@@ -3816,4 +3816,36 @@ public class Top100LikedQuestions {
 		return result;
 	}
 
+	/*
+	 * 128. Longest Consecutive Sequence
+	 * 
+	 * Given an unsorted array of integers, find the length of the longest
+	 * consecutive elements sequence.
+	 * 
+	 * Your algorithm should run in O(n) complexity.
+	 */
+	public int longestConsecutive(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		Arrays.sort(nums);
+
+		int longestStreak = 1;
+		int currentStreak = 1;
+
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[i - 1]) {
+				if (nums[i] == nums[i - 1] + 1) {
+					currentStreak += 1;
+				} else {
+					longestStreak = Math.max(longestStreak, currentStreak);
+					currentStreak = 1;
+				}
+			}
+		}
+
+		return Math.max(longestStreak, currentStreak);
+	}
+
 }
