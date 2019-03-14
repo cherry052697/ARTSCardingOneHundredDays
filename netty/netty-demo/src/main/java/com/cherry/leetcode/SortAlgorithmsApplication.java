c536f0b5166a43342f95e3267e61783020045a8b
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-03-14 10:31:40 +0800

    324. Wiggle Sort II

--- netty/netty-demo/src/main/java/com/cherry/leetcode/SortAlgorithmsApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/SortAlgorithmsApplication.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 import com.cherry.netty.utils.JsonUtil;
 
@@ -325,11 +326,110 @@ public class SortAlgorithmsApplication {
 		return sb.toString();
 	}
 
+	/*
+	 * 324. Wiggle Sort II
+	 * 
+	 * Given an unsorted array nums, reorder it such that nums[0] < nums[1] >
+	 * nums[2] < nums[3]....
+	 */
+	public void wiggleSort(int[] nums) {
+		int n = nums.length, m = (n + 1) >> 1;
+	    int[] copy = Arrays.copyOf(nums, n);
+	    Arrays.sort(copy);
+	    
+	    for (int i = m - 1, j = 0; i >= 0; i--, j += 2) nums[j] = copy[i];
+	    for (int i = n - 1, j = 1; i >= m; i--, j += 2) nums[j] = copy[i];
+	}
+
+	public void wiggleSort2(int[] nums) {
+	    int n = nums.length, m = (n + 1) >> 1;
+	    int median = kthSmallestNumber(nums, m);
+	    
+	    for (int i = 0, j = 0, k = n - 1; j <= k;) {
+	        if (nums[A(j, n)] > median) {
+	            swap(nums, A(i++, n), A(j++, n));
+	        } else if (nums[A(j, n)] < median) {
+	            swap(nums, A(j, n), A(k--, n));
+	        } else {
+	            j++;
+	        }
+	    }
+	}
+	public void wiggleSort3(int[] nums) {
+	    int n = nums.length, m = (n + 1) >> 1;
+	    int[] copy = Arrays.copyOf(nums, n);
+	    int median = kthSmallestNumber(nums, m);
+	    
+	    for (int i = 0, j = 0, k = n - 1; j <= k;) {
+	        if (copy[j] < median) {
+	            swap(copy, i++, j++);
+	        } else if (copy[j] > median) {
+	            swap(copy, j, k--);
+	        } else {
+	            j++;
+	        }
+	    }
+	        
+	    for (int i = m - 1, j = 0; i >= 0; i--, j += 2) nums[j] = copy[i];
+	    for (int i = n - 1, j = 1; i >= m; i--, j += 2) nums[j] = copy[i];
+	}
+	private int kthSmallestNumber(int[] nums, int k) {
+	    Random random = new Random();
+	    
+	    for (int i = nums.length - 1; i >= 0; i--) {
+	        swap(nums, i, random.nextInt(i + 1));
+	    }
+	    
+	    int l = 0, r = nums.length - 1;
+	    k--;
+	        
+	    while (l < r) {
+	        int m = getMiddle(nums, l, r);
+	        
+	        if (m < k) {
+	            l = m + 1;
+	        } else if (m > k) {
+	            r = m - 1;
+	        } else {
+	            break;
+	        }
+	    }
+	    
+	    return nums[k];
+	}
+	    
+	private int getMiddle(int[] nums, int l, int r) {
+	    int i = l;
+	    
+	    for (int j = l + 1; j <= r; j++) {
+	        if (nums[j] < nums[l]) swap(nums, ++i, j);
+	    }
+	    
+	    swap(nums, l, i);
+	    return i;
+	}
+	private void swap(int[] nums, int i, int j) {
+	    int t = nums[i];
+	    nums[i] = nums[j];
+	    nums[j] = t;
+	}
+
+	private int A(int i, int n) {
+	    return (2 * i + 1) % (n | 1);
+	}
+
 	public static void main(String[] args) {
 		SortAlgorithmsApplication saa = new SortAlgorithmsApplication();
-		int[] a = { 10,2 };
+		int[] a = { 1, 5, 1, 1, 6, 4 };
 		// System.out.println(JsonUtil.toJson(saa.sortedSquares(a)));
-		System.out.println(saa.largestNumber(a));
+		saa.wiggleSort(a);
+		System.out.println(JsonUtil.toJson(a));
+		int[] a2 = { 1, 5, 1, 1, 6, 4 };
+		saa.wiggleSort2(a2);
+		System.out.println(JsonUtil.toJson(a2));
+		int[] a3 = { 1, 5, 1, 1, 6, 4 };
+		saa.wiggleSort3(a3);
+		System.out.println(JsonUtil.toJson(a3));
 	}
 
 }
