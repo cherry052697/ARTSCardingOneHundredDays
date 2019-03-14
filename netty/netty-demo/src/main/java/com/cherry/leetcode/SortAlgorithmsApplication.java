2dee36d0bf8a235f56f8766c18838047bb294dd7
liuxiaoyan <liuxiaoyan@brzhongyi.com>
2019-03-14 15:15:34 +0800

    767. Reorganize String

--- netty/netty-demo/src/main/java/com/cherry/leetcode/SortAlgorithmsApplication.java
+++ netty/netty-demo/src/main/java/com/cherry/leetcode/SortAlgorithmsApplication.java
@@ -635,6 +635,19 @@ public class SortAlgorithmsApplication {
 		return sortedHead;
 	}
 
+	/*
+	 * 767. Reorganize String
+	 * 
+	 * Given a string S, check if the letters can be rearranged so that two
+	 * characters that are adjacent to each other are not the same.
+	 * 
+	 * If possible, output any possible result. If not possible, return the
+	 * empty string.
+	 */
+	public String reorganizeString(String S) {
+		return null;
+	}
+
 	public static void main(String[] args) {
 		SortAlgorithmsApplication saa = new SortAlgorithmsApplication();
 		// int[] a = { 1, 5, 1, 1, 6, 4 };
